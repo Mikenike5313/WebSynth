d028c82ff5096402ebc6ab21239451eba4d72d33
Mikenike5313 <michaelkuhn@Michaels-MBP.attlocal.net>
2023-07-27 14:58:50 -0700

    modified knob turining functionality to be more intuitive; before the tick would jump to the angle you grab the knob at, but now the point you grab the knob at is what is anchored to your mouse motions

--- next-app/src/components/gainKnob.js
+++ next-app/src/components/gainKnob.js
@@ -3,26 +3,30 @@
 import styles from '@/styles/knobs.module.css'
 
 function getTransformOrigin(elt) {
-    let elt_style = window.getComputedStyle(elt)
-    let elt_torigin_str_arr = elt_style.getPropertyValue("transform-origin").split("px");
-    let elt_top_str_arr = elt_style.getPropertyValue("top").split("px");
-    let elt_left_str_arr = elt_style.getPropertyValue("left").split("px");
-    return [parseFloat(elt_torigin_str_arr[0])+parseFloat(elt_left_str_arr[0]), parseFloat(elt_torigin_str_arr[1]+parseFloat(elt_top_str_arr[0]))];
+    let eltStyle = window.getComputedStyle(elt)
+    let eltTOriginStrArr = eltStyle.getPropertyValue("transform-origin").split("px");
+    let eltTopStrArr = eltStyle.getPropertyValue("top").split("px");
+    let eltLeftStrArr = eltStyle.getPropertyValue("left").split("px");
+    return [parseFloat(eltTOriginStrArr[0])+parseFloat(eltLeftStrArr[0]), parseFloat(eltTOriginStrArr[1]+parseFloat(eltTopStrArr[0]))];
 }
 
-function turn(elt, e) {
-    let tick = elt.querySelector("."+styles.tick);
-    let origin = getTransformOrigin(tick);
-    let angle = Math.atan2(e.clientX-origin[0], origin[1]-e.clientY)*180/Math.PI;
+function turnKnob(initials, elt, e) {
+    let origin = getTransformOrigin(elt);
+    let grabAngle = Math.atan2(initials.mx-origin[0], origin[1]-initials.my)*180/Math.PI;
+    let moveAngle = Math.atan2(e.clientX-origin[0], origin[1]-e.clientY)*180/Math.PI;
+    let angle = (moveAngle-grabAngle+initials.angle+540)%360 - 180;
+    console.log(angle);
     angle = (Math.abs(angle) > 135) ? 135*angle/Math.abs(angle) : angle;
-    tick.style.transform = "rotate("+angle+"deg)"
+    elt.style.transform = "rotate("+angle+"deg)"
 }
 
 function fiddle(evt) {
-    let fiddle_turn = function(e) {turn(evt.target, e)};
-    window.addEventListener("mousemove", fiddle_turn);
+    let knob = evt.target.querySelector("."+styles.tick);
+    let initialAngle = knob.style.transform == "" ? 0 : parseFloat(knob.style.transform.match(/[+-]?[0-9]+[.]?[0-9]*/));
+    let fiddleTurn = function(e) {turnKnob({angle:initialAngle, mx:evt.clientX, my:evt.clientY}, knob, e)};
+    window.addEventListener("mousemove", fiddleTurn);
     window.addEventListener("mouseup", () => {
-        window.removeEventListener("mousemove", fiddle_turn);
+        window.removeEventListener("mousemove", fiddleTurn);
     });
 }
 
